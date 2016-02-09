9a962f82bd9a5d5d87112a29fbfb8e8d81edff78
Christophe MICHEL <christophe.michel.arolla@ext.pmu.fr>
2016-02-09 14:51:52 +0100

    Simple wrap case.

--- src/main/java/kata/wordwrap/WordWrap.java
+++ src/main/java/kata/wordwrap/WordWrap.java
@@ -6,7 +6,25 @@ public final class WordWrap {
     }
 
     public static String wrap(String sentence, int width) {
-        return sentence;
+        if (sentence.length() <= width) {
+            return sentence;
+        }
+
+        return actualWrap(sentence, width);
+    }
+
+    private static String actualWrap(String sentence, int width) {
+        StringBuilder result = new StringBuilder();
+
+        int position = width;
+        while (position < sentence.length()) {
+            result.append(sentence.substring(position - width, position));
+            result.append("\n");
+            position += width;
+        }
+        result.append(sentence.substring(position - width));
+
+        return result.toString();
     }
 
 }
--- src/test/java/kata/wordwrap/WordWrapTest.java
+++ src/test/java/kata/wordwrap/WordWrapTest.java
@@ -7,9 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WordWrapTest {
 
     @Test
-    public void test_no_wrap() {
+    public void should_not_wrap_if_sentence_smaller_than_width() {
         String result = WordWrap.wrap("a simple test", 200);
         assertThat(result).isEqualTo("a simple test");
+    }
+
+    @Test
+    public void should_wrap_inside_word_if_word_larger_than_with() {
+        String result = WordWrap.wrap("absolutely", 4);
+        assertThat(result).isEqualTo("abso\nlute\nly");
     }
 
 }
